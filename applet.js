e3d3f61803562808ad2988fd8249f3f1bb3af30f
rybick <bzareba21@gmail.com>
2025-12-28 14:30:12 +0100

    icons

--- applet.js
+++ applet.js
@@ -11,8 +11,8 @@ MyApplet.prototype = {
     _init: function(orientation, panel_height, instance_id) {
         Applet.IconApplet.prototype._init.call(this, orientation, panel_height, instance_id);
 
-        this.set_applet_icon_name("force-exit");
-        this.set_applet_tooltip(_("Click here to kill a window"));
+        this.set_applet_icon_symbolic_name("ksa-neutral");
+        this.set_applet_tooltip(_("Everything synced-up"));
     },
 
     on_applet_clicked: function() {
--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 path=~/.local/share/cinnamon/applets/keepass-sync-applet@rybick.github.io
 mkdir -p $path
+rm -rf $path/*
 cp metadata.json $path/
 cp applet.js $path/
+cp -r icons/ $path/
