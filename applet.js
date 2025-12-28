ab762123381793cdcd9204e38ca6d0db9c85d8ee
rybick <bzareba21@gmail.com>
2025-12-28 14:49:56 +0100

    dummy context menu

--- applet.js
+++ applet.js
@@ -1,4 +1,5 @@
 const Applet = imports.ui.applet;
+const PopupMenu = imports.ui.popupMenu;
 const Util = imports.misc.util;
 
 function MyApplet(orientation, panel_height, instance_id) {
@@ -13,16 +14,34 @@ MyApplet.prototype = {
 
         this.set_applet_icon_symbolic_name("ksa-neutral");
         this.set_applet_tooltip(_("Everything synced-up"));
+
+        this.menuManager = new PopupMenu.PopupMenuManager(this);
+        this.menu = createMenu(this, orientation);
+        this.menuManager.addMenu(this.menu);
     },
 
     on_applet_clicked: function() {
         global.log("Applet clicked");
-        Util.trySpawnCommandLine('xed');
-        //Util.trySpawnCommandLine('echo "x1" >> ~/doc/tmp/test.txt');
-	    // Util.spawn(['xkill']);
-        // alert('I was clicked');
+        this.menu.toggle();
     }
 };
+
+function createMenu(launcher, orientation) {
+    let menu = new Applet.AppletPopupMenu(launcher, orientation);
+    let item1 = new PopupMenu.PopupMenuItem("Option #1");
+    item1.connect('activate', () => {
+        //Util.spawnCommandLine('xkill');
+    });
+    menu.addMenuItem(item1);
+
+    let item2 = new PopupMenu.PopupMenuItem("Option #2");
+    item2.connect('activate', () => {
+        //global.log("Settings clicked!");
+    });
+    menu.addMenuItem(item2);
+
+    return menu;
+}
 
 function main(metadata, orientation, panel_height, instance_id) {
     return new MyApplet(orientation, panel_height, instance_id);
